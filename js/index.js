aeb902a5bf87ad7482f1d7ccd273f8bca1882a98
derrick b <dbozich1@gmail.com>
2018-10-11 23:12:33 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function getRepositories(){
   const req = new XMLHttpRequest();
 
   //callback that invokes showRepositories once the data loads
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open("GET", `https://api.github.com/users/${username}/repos`);
   req.send();
 }
