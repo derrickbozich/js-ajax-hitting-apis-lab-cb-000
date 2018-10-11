41c2a49b9372ec9f04e009636864eedd6b2046fa
derrick b <dbozich1@gmail.com>
2018-10-11 20:47:55 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -2,15 +2,21 @@
 function getRepositories(){
   const username = document.getElementById('username').value;
 
+  //creates a new XMLHttpRequest object
   const req = new XMLHttpRequest();
 
+  //callback that invokes showRepositories once the data loads
   req.addEventListener('load', showRepositories);
   req.open("GET", `https://api.github.com/users/${username}/repos`);
   req.send();
 }
 
 function showRepositories(){
+  //parses response into JSON
   let repos = JSON.parse(this.responseText);
   console.log(repos);
+
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`;
   debugger
+  
 }
