524578e51887eb620b571b6328fddf0b3e274815
derrick b <dbozich1@gmail.com>
2018-10-11 21:22:01 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -16,6 +16,18 @@ function showRepositories(){
   let repos = JSON.parse(this.responseText);
   console.log(repos);
 
-  const repoList = `<ul>${repos.map(r => '<li>' + '<a href=' + r.html_url + '>'  + r.name + '</a>' + ' ' + '<a href="#" onclick=getCommits()>Get Commits</a>' + '</li>').join('')}</ul>`;
+  const repoList = `<ul>${repos.map(r => '<li>' + '<a href=' + r.html_url + '>'  + r.name + '</a>' + ' ' + '<a href="#" data-repo=' + r.name + ' ' + 'onclick=getCommits(this)>Get Commits</a>' + '</li>').join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(){
+  const username = document.getElementById('username').value;
+
+  //creates a new XMLHttpRequest object
+  const req = new XMLHttpRequest();
+
+  //callback that invokes showRepositories once the data loads
+  req.addEventListener('load', showRepositories);
+  req.open("GET", `https://api.github.com/users/${username}/repos/:owner/:repo/commits`);
+  req.send();
 }
