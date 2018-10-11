b5f22d14b50c6397bde1ab532f00d284e125409c
derrick b <dbozich1@gmail.com>
2018-10-11 20:33:00 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-// your code here
+// fetches repositories from github
+function getRepositories(){
+  const username = document.getElementById('username');
+
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open("GET", `https://api.github.com/users/${username}/repos`);
+  req.send();
+}
+
+function showRepositories(){
+  let repos = JSON.parse(this.responseText);
+  console.log(repos);
+  
+}
