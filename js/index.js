63f79395281f0b26226fa956d0552198a9b5c40f
derrick b <dbozich1@gmail.com>
2018-10-11 21:53:48 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ function showRepositories(){
   //parses response into JSON
   let repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `<ul>${repos.map(r => '<li>' + '<a href=' + r.html_url + '>'  + r.name + '</a>' + ' - ' + '<a href="#" data-repo=' + r.name + ' ' + 'onclick=getCommits(this)>Get Commits</a>' + '</li>').join('')}</ul>`;
+  const repoList = `<ul>${repos.map(r => '<li>' + '<a href=' + r.html_url + '>'  + r.name + '</a>' + ' - ' + '<a href="#" data-repo=' + r.name + ' ' + 'data-username=' + r.owner.login + 'onclick=getCommits(this)>Get Commits</a>' + '</li>').join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
 
@@ -38,5 +38,5 @@ function displayCommits(){
   const commitList = `<ul>${commits.map(c => '<li>' + '<a href=' + c.html_url + '>'  + c.name + '</a>' + ' - ' + '<a href="#" data-repo=' + c.name + ' ' + 'onclick=getCommits(this)>Get Commits</a>' + '</li>').join('')}</ul>`;
 
 
-  
+
 }
